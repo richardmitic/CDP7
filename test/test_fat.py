2dabe010a1354ea8cc1c0a68565327f29f997af3
Richard Mitic <richard.h.mitic@gmail.com>
2017-03-21 21:06:04 +0100

    Remove dummy test from TestFat

--- test/test_fat.py
+++ test/test_fat.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 def _is_fat(program):
-    txt = str(subprocess.check_output(["lipo", "-info", program]), 'utf8')
+    txt = str(subprocess.check_output(["lipo", "-info", program]))
     regex = r"(i386 x86_64|x86_64 i386)"
     return bool(re.search(regex, txt))
 
@@ -17,24 +17,19 @@ def make_test(program):
     return test
 
 class meta_TestFat(type):
-  @classmethod
-  def __prepare__(mcls, name, bases):
-    d = dict()
-    for root, dirs, files in os.walk("/Users/richard/Developer/CDP7/Release"):
-        programs = [os.path.join(root, f) for f in files]
-        for p in programs:
-            test_name = "test_fat_{}".format(p)
-            test = make_test(p)
-            d[test_name] = test
-    return d
-
+    @classmethod
+    def __prepare__(mcls, name, bases):
+        d = dict()
+        for root, dirs, files in os.walk("/Users/richard/Developer/CDP7/Release"):
+            programs = [os.path.join(root, f) for f in files]
+            for p in programs:
+                test_name = "test_fat_{}".format(p)
+                test = make_test(p)
+                d[test_name] = test
+        return d
 
 class TestFat(unittest.TestCase, metaclass=meta_TestFat):
-    @unittest.skip("Class must have at least one test")
-    def test_thing(self):
-        pass
-
-
+    pass
 
 
 if __name__ == '__main__':
